69679d15a0f0d0cdab72e23d660b26493d6a1e4d
efvai <jkcvkdslf@gmail.com>
2024-02-20 23:21:05 +0400

    curve data

--- qwt-chart/qwtchartplot.cpp
+++ qwt-chart/qwtchartplot.cpp
@@ -6,6 +6,8 @@
 #include <QDebug>
 #include <QEvent>
 #include <QResizeEvent>
+#include <QTimer>
+#include <QtMath>
 
 #include <QwtPlotGrid>
 #include <QwtPlotLayout>
@@ -13,6 +15,51 @@
 #include <QwtInterval>
 #include <QwtPlotMagnifier>
 #include <QwtPlotCurve>
+#include <QwtPlotScaleItem>
+
+namespace {
+    class CurveData : public QwtSeriesData<QPointF>
+    {
+    public:
+        virtual QPointF sample(size_t index) const QWT_OVERRIDE {
+            return m_data.at(index);
+        }
+
+        virtual size_t size() const QWT_OVERRIDE {
+            return m_data.size();
+        }
+
+        virtual QRectF boundingRect() const QWT_OVERRIDE {
+            return m_boundingRect;
+        }
+
+
+        CurveData() {
+            m_data.reserve(1000);
+            m_boundingRect = QRectF(1.0, 1.0, -2.0, -2.0);
+        }
+
+        void appendData(const QPointF &point) {
+            m_data.append(point);
+            if (m_boundingRect.width() < 0 || m_boundingRect.height() < 0) {
+                m_boundingRect.setRect(point.x(), point.y(), 0.0, 0.0);
+            } else {
+                m_boundingRect.setRight(point.x());
+                if (point.y() > m_boundingRect.bottom()) {
+                    m_boundingRect.setBottom(point.y());
+                }
+                if (point.y() < m_boundingRect.top()) {
+                    m_boundingRect.setTop(point.y());
+                }
+            }
+        }
+
+    private:
+        QList<QPointF> m_data;
+        QRectF m_boundingRect;
+
+    };
+}
 
 QwtChartPlot::QwtChartPlot(QWidget *canvas)
     : QwtPlot(nullptr)
@@ -35,23 +82,44 @@ QwtChartPlot::QwtChartPlot(QWidget *canvas)
 
     (void) new QwtChartMagnifier(canvas);
 
-    QVector<QPointF> points;
-    for (int i = 0; i < 100; i++) {
-        points.append(QPointF(i, i%4));
-    }
-    QwtPlotCurve *curve = new QwtPlotCurve();
-    curve->setPen(Qt::blue, 3);
-    curve->setRenderHint(QwtPlotItem::RenderAntialiased, true);
-    curve->setSamples(points);
-    curve->attach(this);
-
-    m_dataInterval = QwtInterval(points.first().x(), points.last().x());
-    m_visibleInterval = QwtInterval(points.last().x()
-                                        - m_visibleIntervalLength, points.last().x());
+    m_dataInterval = QwtInterval(0, 5);
+    m_visibleInterval = QwtInterval(0, 5);
     setAxisScale(QwtAxis::XBottom, m_visibleInterval.minValue(),
                     m_visibleInterval.maxValue());
 
-    updateScrollBar();
+    static QwtPlotScaleItem *scaleItem = new QwtPlotScaleItem( QwtScaleDraw::RightScale, 2 );
+    scaleItem->setFont( this->axisWidget( QwtAxis::YLeft )->font() );
+    scaleItem->attach(this);
+
+    this->setAxisVisible( QwtAxis::YLeft, false );
+
+    static QwtPlotCurve *curve = new QwtPlotCurve;
+    curve->setData(new CurveData);
+    curve->setRenderHint(QwtPlotItem::RenderAntialiased);
+    curve->setPaintAttribute(QwtPlotCurve::FilterPoints);
+    curve->setPen(Qt::blue, 2);
+    curve->attach(this);
+
+    QTimer *timer = new QTimer;
+    connect(timer, &QTimer::timeout,
+            this, [&]() {
+        static float dx = 0;
+        if (dx < 25) {
+            QPointF p(dx, 3.0 * qSin(dx * 5));
+            dx += 0.01;
+            CurveData *curveData = static_cast<CurveData*>(curve->data());
+            curveData->appendData(p);
+            m_dataInterval.setMaxValue(dx);
+            if (dx > m_visibleIntervalLength) {
+                m_visibleInterval.setMaxValue(dx);
+                m_visibleInterval.setMinValue(dx - m_visibleIntervalLength);
+            }
+            setAxisScale(QwtAxis::XBottom, m_visibleInterval.minValue(), m_visibleInterval.maxValue());
+            scaleItem->setPosition((m_visibleInterval.minValue() + m_visibleInterval.maxValue())*0.5);
+            replot();
+        }
+    });
+    timer->start(5);
     replot();
 }
 
@@ -87,8 +155,8 @@ bool QwtChartPlot::eventFilter(QObject *object, QEvent *event)
 
 void QwtChartPlot::updateScrollBar()
 {
-    qDebug() << "interval: " <<axisInterval(QwtAxis::XBottom).width();
-    m_visibleInterval = axisInterval(QwtAxis::XBottom);
+
+
     if (!canvas()) {
         return;
     }
